65e66e4e6c77a4d61a0b29e47190444a086662e1
TerminalSwagDisorder <44695050+TerminalSwagDisorder@users.noreply.github.com>
2023-02-08 00:09:45 +0200

    Changed space indentation to tab indentation

--- Exercise_3/code/dice.py
+++ Exercise_3/code/dice.py
@@ -6,54 +6,54 @@ import random
 
 # Class for dice
 class Dice:
-    def __init__(self):
-        self.face = "1"
-        self.color = "White"
-        self.material = "Plastic"
-        
-    def roll(self):
-        rng = random.randint(0, 5)
-        if rng == 0:
-            self.face = "1"
-        elif rng == 1:
-            self.face = "2"
-        elif rng == 2:
-            self.face = "3"
-        elif rng == 3:
-            self.face = "4"
-        elif rng == 4:
-            self.face = "5"
-        else:
-            self.face = "6"
-    
-    def get_face(self):
-        return self.face
-    
-    def paint(self):
-        rng = random.randint(0, 3)
-        if rng == 0:
-            self.color = "White"
-        elif rng == 1:
-            self.color = "Red"
-        elif rng == 2:
-            self.color = "Green"
-        else:
-            self.color = "Blue"
-    
-    def get_color(self):
-        return self.color
-    
-    def transmogrify(self):
-        rng = random.randint(0, 3)
-        if rng == 0:
-            self.material = "Plastic"
-        elif rng == 1:
-            self.material = "Bone"
-        elif rng == 2:
-            self.material = "Metal"
-        else:
-            self.material = "Foam"
-    
-    def get_material(self):
-        return self.material
-    
+	def __init__(self):
+		self.face = "1"
+		self.color = "White"
+		self.material = "Plastic"
+		
+	def roll(self):
+		rng = random.randint(0, 5)
+		if rng == 0:
+			self.face = "1"
+		elif rng == 1:
+			self.face = "2"
+		elif rng == 2:
+			self.face = "3"
+		elif rng == 3:
+			self.face = "4"
+		elif rng == 4:
+			self.face = "5"
+		else:
+			self.face = "6"
+	
+	def get_face(self):
+		return self.face
+	
+	def paint(self):
+		rng = random.randint(0, 3)
+		if rng == 0:
+			self.color = "White"
+		elif rng == 1:
+			self.color = "Red"
+		elif rng == 2:
+			self.color = "Green"
+		else:
+			self.color = "Blue"
+	
+	def get_color(self):
+		return self.color
+	
+	def transmogrify(self):
+		rng = random.randint(0, 3)
+		if rng == 0:
+			self.material = "Plastic"
+		elif rng == 1:
+			self.material = "Bone"
+		elif rng == 2:
+			self.material = "Metal"
+		else:
+			self.material = "Foam"
+	
+	def get_material(self):
+		return self.material
+	
